38183e3e5b148476a75e80b8d2f43bfb5833e24d
shahzad-ishaq <46834835+shahzad-ishaq@users.noreply.github.com>
2023-05-17 10:20:11 +0500

    Update dbconnection.php
    remove the live db connection

--- dbconnection.php
+++ dbconnection.php
@@ -1,9 +1,6 @@
 <?php
 
-$link = mysqli_connect('localhost', 'root', '', 'production');
-//$link = mysqli_connect('kipsrdsnew.clboc0xjvbaa.us-east-1.rds.amazonaws.com', 'kips_rds_admin', 'Kips$Admin$', 'kips_production');
-///$link = mysqli_connect('kips-productiondb.clboc0xjvbaa.us-east-1.rds.amazonaws.com', 'kips_rds_admin', 'Kips$Admin$', 'kips_production');
-
+$link = mysqli_connect('localhost', 'root', '', 'databasename');
 if (!$link) {
     die('Connect Error (' . mysqli_connect_errno() . ') '
             . mysqli_connect_error());
